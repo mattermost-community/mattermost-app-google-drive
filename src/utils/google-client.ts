9ab36ecceb81b5ce4302c6277e3d41bc4a152971
Lizeth García <lizeth.garcia@ancient.mx>
2022-08-25 17:43:37 -0500

    feat: manage 'getGoogleDriveClient' with both options: from context and from webhook (MATT-115)

--- src/utils/google-client.ts
+++ src/utils/google-client.ts
@@ -27,17 +27,19 @@ export const getGoogleDriveClient = async (call: AppCallRequest): Promise<any> =
    const mattermostUrl: string | undefined = call.context.mattermost_site_url;
    const botAccessToken: string | undefined = call.context.bot_access_token;
    const userID: string | undefined = call.context.acting_user?.id;
+   let oauth2Token = call.context.oauth2?.user as Oauth2CurrentUser;
+
+   if (!oauth2Token?.refresh_token) {
+      const kvOptions: KVStoreOptions = {
+         mattermostUrl: <string>mattermostUrl,
+         accessToken: <string>botAccessToken
+      };
+      const kvStoreClient = new KVStoreClient(kvOptions);
+      oauth2Token = await kvStoreClient.kvGet(<string>userID);
+   }
+
    const oauth2Client = await getOAuthGoogleClient(call);
-
-   const kvOptions: KVStoreOptions = {
-      mattermostUrl: <string>mattermostUrl,
-      accessToken: <string>botAccessToken
-   };
-   const kvStoreClient = new KVStoreClient(kvOptions);
-   const userToken: Oauth2CurrentUser = await kvStoreClient.kvGet(<string>userID);
-
-   oauth2Client.setCredentials(userToken);
-
+   oauth2Client.setCredentials(oauth2Token);
    await tryPromise(oauth2Client.refreshAccessToken(), ExceptionType.MARKDOWN, 'Google failed: ');
 
    return google.drive({
