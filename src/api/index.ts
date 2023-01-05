295cf1c212d7931cfd516e017dcd2e7e23d1929a
Lizeth García <lizeth.garcia@ancient.mx>
2023-01-05 12:20:38 -0600

    feat: added oauth middleware and added to methods that execute a submit method or needs the user to have OAuth configured (pr-mattermost-app-google-drive-2)

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import express, { Router } from 'express';
 
 import { Routes } from '../constant';
 
-import { requireSystemAdmin } from '../restapi/middleware';
+import { requireUserOAuthConnected, requireSystemAdmin } from '../restapi/middleware';
 
 import * as cManifest from './manifest';
 import * as cBindings from './bindings';
@@ -32,35 +32,35 @@ router.post(Routes.App.CallPathConfigSubmit, requireSystemAdmin, cConfigure.conf
 router.post(`${Routes.App.CallPathConnectSubmit}`, cConnect.getConnectGoogleURL);
 router.post(`${Routes.App.OAuthConnectPath}`, cConnect.fOauth2Connect);
 router.post(`${Routes.App.OAuthCompletePath}`, cConnect.fOauth2Complete);
-router.post(`${Routes.App.CallPathDisconnectSubmit}`, cConnect.doDisconnectGoogle);
+router.post(`${Routes.App.CallPathDisconnectSubmit}`, requireUserOAuthConnected, cConnect.doDisconnectGoogle);
 
 // Stop or start Google's notifications
-router.post(`${Routes.App.CallPathStartNotifications}`, cNotifications.startGoogleNotifications);
-router.post(`${Routes.App.CallPathStopNotifications}`, cNotifications.stopGoogleNotifications);
+router.post(`${Routes.App.CallPathStartNotifications}`, requireUserOAuthConnected, cNotifications.startGoogleNotifications);
+router.post(`${Routes.App.CallPathStopNotifications}`, requireUserOAuthConnected, cNotifications.stopGoogleNotifications);
 
 // Receive notifications
 router.post(`${Routes.App.CallPathIncomingWebhookPath}`, cWebhook.filterWebhookNotification);
 
 // Reply to comments
 router.post(`${Routes.App.CallPathCommentReplayForm}`, cReplyComments.replyToCommentForm);
-router.post(`${Routes.App.CallPathCommentReplaySubmit}`, cReplyComments.replyToCommentSubmit);
+router.post(`${Routes.App.CallPathCommentReplaySubmit}`, requireUserOAuthConnected, cReplyComments.replyToCommentSubmit);
 
 // Create new Google Files
 router.post(`${Routes.App.CallPathCreateDocument}`, cGoogleFiles.openFormGoogleDocs);
 router.post(`${Routes.App.CallPathUpdateDocumentForm}`, cGoogleFiles.openFormGoogleDocs);
-router.post(`${Routes.App.CallPathCreateDocumentSubmit}`, cGoogleFiles.executeFormGoogleDocs);
+router.post(`${Routes.App.CallPathCreateDocumentSubmit}`, requireUserOAuthConnected, cGoogleFiles.executeFormGoogleDocs);
 
 router.post(`${Routes.App.CallPathCreateSpreadsheet}`, cGoogleFiles.openFormGoogleSheets);
 router.post(`${Routes.App.CallPathUpdateSpreadsheetForm}`, cGoogleFiles.openFormGoogleSheets);
-router.post(`${Routes.App.CallPathCreateSpreadsheetSubmit}`, cGoogleFiles.executeFormGoogleSheets);
+router.post(`${Routes.App.CallPathCreateSpreadsheetSubmit}`, requireUserOAuthConnected, cGoogleFiles.executeFormGoogleSheets);
 
 router.post(`${Routes.App.CallPathCreatePresentation}`, cGoogleFiles.openFormGoogleSlides);
 router.post(`${Routes.App.CallPathUpdatePresentationForm}`, cGoogleFiles.openFormGoogleSlides);
-router.post(`${Routes.App.CallPathCreatePresentationSubmit}`, cGoogleFiles.executeFormGoogleSlides);
+router.post(`${Routes.App.CallPathCreatePresentationSubmit}`, requireUserOAuthConnected, cGoogleFiles.executeFormGoogleSlides);
 
 // Upload files to Drive
 router.post(`${Routes.App.CallPathSaveFileCall}`, cUploadFile.uploadFileToDriveCall);
-router.post(`${Routes.App.CallPathSaveFileSubmit}`, cUploadFile.uploadFileToDriveSubmit);
+router.post(`${Routes.App.CallPathSaveFileSubmit}`, requireUserOAuthConnected, cUploadFile.uploadFileToDriveSubmit);
 
 const staticRouter = express.Router();
 staticRouter.use(express.static('static'));
--- src/restapi/middleware.ts
+++ src/restapi/middleware.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
 
-import { AppActingUser, ExtendedAppCallRequest } from '../types';
+import { AppActingUser, ExtendedAppCallRequest, Oauth2App } from '../types';
 import { newErrorCallResponseWithMessage } from '../utils/call-responses';
 import { configureI18n } from '../utils/translations';
-import { isUserSystemAdmin } from '../utils/utils';
+import { isConnected, isUserSystemAdmin } from '../utils/utils';
 
 export const requireSystemAdmin = (req: Request, res: Response, next: () => void) => {
     const call: ExtendedAppCallRequest = req.body as ExtendedAppCallRequest;
@@ -17,6 +17,19 @@ export const requireSystemAdmin = (req: Request, res: Response, next: () => void
 
     if (!isUserSystemAdmin(actingUser)) {
         res.json(newErrorCallResponseWithMessage(i18nObj.__('general.validation-user.system-admin')));
+        return;
+    }
+
+    next();
+};
+
+export const requireUserOAuthConnected = (req: Request, res: Response, next: () => void) => {
+    const call: ExtendedAppCallRequest = req.body as ExtendedAppCallRequest;
+    const i18nObj = configureI18n(call.context);
+    const oauth2: Oauth2App = call.context.oauth2 as Oauth2App;
+
+    if (!isConnected(oauth2)) {
+        res.json(newErrorCallResponseWithMessage(i18nObj.__('general.validation-user.oauth-user')));;
         return;
     }
 
