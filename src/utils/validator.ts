60756da1999e9cd1a39d5beda72912ade455c187
Felix Alejandro Tonatiuh Gomez Briones <felix.gomez@ancient.mx>
2023-01-31 19:49:57 -0600

    feat: [MATT-226] Validator settings

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -4,26 +4,11 @@ export const AppFormValidator = z.object({
     title: z.string(),
     header: z.string().optional(),
     icon: z.string(),
-    fields: z.tuple([
-        z.object({
-            type: z.string(),
-            name: z.string(),
-            subtype: z.string(),
-            modal_label: z.string(),
-            description: z.string(),
-            is_required: z.boolean(),
-        }),
-    ]).optional(),
+    fields: z.array(z.any()).optional(),
     submit: z.object({
         path: z.string(),
-        expand: z.object({
-            acting_user: z.string(),
-            acting_user_access_token: z.string(),
-            oauth2_user: z.string(),
-            oauth2_app: z.string(),
-            post: z.string(),
-        }),
-        state: z.record(z.any()).optional(),
+        expand: z.any(),
+        state: z.any().optional(),
     }),
     source: z.object({
         path: z.string(),
@@ -38,43 +23,10 @@ export const AppFormValidator = z.object({
 export const ExtendedAppFormValidator = z.object({
     title: z.string(),
     icon: z.string(),
-    fields: z.tuple([
-        z.object({
-            name: z.string(),
-            type: z.string(),
-            is_required: z.boolean().optional(),
-            readonly: z.boolean().optional(),
-            value: z.string().optional(),
-            description: z.string().optional(),
-            label: z.string().optional(),
-            hint: z.string().optional(),
-            position: z.number().int().optional(),
-            modal_label: z.string().optional(),
-            refresh: z.boolean().optional(),
-            options: z.tuple([
-                z.object({
-                    label: z.string(),
-                    value: z.string(),
-                    icon_data: z.string(),
-                }),
-            ]).optional(),
-            multiselect: z.boolean().optional(),
-            lookup: z.string().optional(),
-            subtype: z.string().optional(),
-            min_length: z.number().int().optional(),
-            max_length: z.number().int().optional(),
-        }),
-    ]).optional(),
+    fields: z.array(z.any()).optional(),
     submit: z.object({
         path: z.string(),
-        expand: z.object({
-            acting_user: z.string(),
-            acting_user_access_token: z.string(),
-            oauth2_app: z.string(),
-            oauth2_user: z.string(),
-            channel: z.string(),
-            locale: z.string(),
-        }),
+        expand: z.any(),
     }),
     source: z.object({
         path: z.string(),
